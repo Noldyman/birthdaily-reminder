665cbddff43a62edb84755760ea8365de45d9fbc
Noud Verstijnen <noud.verstijnen@gmail.com>
2024-03-13 07:57:20 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+require("dotenv").config();
+import { Birthday } from "./models/birthday";
+import schedule from "node-schedule";
+
+const env = {
+  serverBaseUrl: process.env.SERVER_BASE_URL,
+  homepageApiPort: process.env.HOMEPAGE_API_PORT,
+  hassPort: process.env.HASS_PORT,
+  hassAccessToken: process.env.HASS_ACCESS_TOKEN,
+};
+
+const getTodaysBirthdays = async (): Promise<Birthday[]> => {
+  const response = await fetch(
+    `${env.serverBaseUrl}:${env.homepageApiPort}/api/birthdays?interval=day`
+  );
+  const data: Birthday[] = await response.json();
+  return data;
+};
+
+const sendBirthdayReminder = async () => {
+  const birthdays = await getTodaysBirthdays();
+  if (!birthdays.length) return;
+
+  const payload = {
+    title: "Birthdaily reminder",
+    message: `Todays birthdays: ${birthdays.map((b) => `${b.name} (${b.newAge})`)}.`,
+  };
+
+  await fetch(`${env.serverBaseUrl}:${env.hassPort}/api/services/notify/mobile_app_iphone_6`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${env.hassAccessToken}`,
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+schedule.scheduleJob("30 9 * * *", sendBirthdayReminder);
---
+++ src/models/birthday.ts
@@ -0,0 +1,6 @@
+export interface Birthday {
+  id: number;
+  name: string;
+  dateOfBirth: string;
+  newAge: number;
+}
